043c9f2ad69ea1815c9db947eaa9ef616481bf29
reshma raghavan <u1430631@umail.utah.edu>
2025-12-07 19:52:34 -0700

    Simplified app.py

--- tethysapp-hello_world/tethysapp/hello_world/app.py
+++ tethysapp-hello_world/tethysapp/hello_world/app.py
@@ -1,47 +1,28 @@
 from tethys_sdk.base import TethysAppBase, url_map_maker
 
 class App(TethysAppBase):
-    """
-    Tethys app class for Hello World.
-    """
-    name = 'Hello World'  # Human-readable app name
-    description = 'Hello World Tethys app for demonstrating map and GeoJSON fetching from S3.'
-    package = 'hello_world'  # WARNING: Do not change this value
+    name = 'Hello World'
+    package = 'hello_world'
     index = 'home'
     icon = f'{package}/images/icon.gif'
-    root_url = 'hello-world'  # URL prefix for the app
+    root_url = 'hello-world'
     color = '#00FFFF'
-    tags = 'Water Demand, WRLU, Residential Irrigation, Hydrology, Water Management, Planning Tools, Decision Support, Utah'
+    description = ''
     enable_feedback = False
     feedback_emails = []
 
     def url_maps(self):
-        """
-        Define URL mappings for the app.
-        """
         UrlMap = url_map_maker(self.root_url)
-
         url_maps = (
-            # Home page (root of app)
             UrlMap(
                 name='home',
-                url='home',  # Root URL for the app
+                url='',
                 controller='hello_world.controllers.home_page'
             ),
-
-            # Map page
             UrlMap(
                 name='map',
-                url='map',  # Appends to root_url -> /apps/hello-world/map
+                url='map',
                 controller='hello_world.controllers.map_page'
             ),
-
-            # GeoJSON endpoint (used by JS to fetch data dynamically)
-            UrlMap(
-                name='geojson',
-                url='geojson/',  # Appends to root_url -> /apps/hello-world/geojson
-                controller='hello_world.controllers.serve_geojson'
-            ),
         )
-
         return url_maps
