2faa28f7094e4ff913c9100bd3e64d49dce97e11
reshma raghavan <u1430631@umail.utah.edu>
2025-12-07 19:55:51 -0700

    Updated app.py to point to a valid URL name

--- tethysapp-hello_world/tethysapp/hello_world/app.py
+++ tethysapp-hello_world/tethysapp/hello_world/app.py
@@ -3,19 +3,15 @@ from tethys_sdk.base import TethysAppBase, url_map_maker
 class App(TethysAppBase):
     name = 'Hello World'
     package = 'hello_world'
-    index = 'home'
-    icon = f'{package}/images/icon.gif'
+    index = 'home'  # This must match the UrlMap name
     root_url = 'hello-world'
-    color = '#00FFFF'
-    description = ''
-    enable_feedback = False
-    feedback_emails = []
+    icon = f'{package}/images/icon.gif'
 
     def url_maps(self):
         UrlMap = url_map_maker(self.root_url)
         url_maps = (
             UrlMap(
-                name='home',
+                name='home',  # Must match index
                 url='',
                 controller='hello_world.controllers.home_page'
             ),
