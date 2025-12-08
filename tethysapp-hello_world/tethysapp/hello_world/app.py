c53b17b9b26307726acf4cd87ce4fe1d35ed563b
reshma raghavan <u1430631@umail.utah.edu>
2025-12-07 20:01:47 -0700

    Single page app

--- tethysapp-hello_world/tethysapp/hello_world/app.py
+++ tethysapp-hello_world/tethysapp/hello_world/app.py
@@ -1,24 +1,20 @@
 from tethys_sdk.base import TethysAppBase, url_map_maker
 
-class App(TethysAppBase):
+class HelloWorld(TethysAppBase):
     name = 'Hello World'
     package = 'hello_world'
-    index = 'home'  # This must match the UrlMap name
+    index = 'home'          # Home page is the index
     root_url = 'hello-world'
     icon = f'{package}/images/icon.gif'
 
     def url_maps(self):
         UrlMap = url_map_maker(self.root_url)
+
         url_maps = (
             UrlMap(
-                name='home',  # Must match index
+                name='home',
                 url='',
                 controller='hello_world.controllers.home_page'
-            ),
-            UrlMap(
-                name='map',
-                url='map',
-                controller='hello_world.controllers.map_page'
             ),
         )
         return url_maps
--- tethysapp-hello_world/tethysapp/hello_world/controllers.py
+++ tethysapp-hello_world/tethysapp/hello_world/controllers.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
 def home_page(request):
-    """Home page view"""
+    """Home page with map and chart"""
     return render(request, 'hello_world/home.html')
-
-def map_page(request):
-    """Map and plot page view"""
-    return render(request, 'hello_world/map.html')
